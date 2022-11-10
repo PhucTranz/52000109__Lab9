a024859adac524011eb6f90564ede20b712bbf8b
admin <admin@602-08>
2022-11-10 08:50:27 +0700

    Add project files.

---
+++ Test/UnitTest1.cs
@@ -0,0 +1,32 @@
+using StudentService;
+
+namespace Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void StudentScore_Should_Success()
+        {
+            Student st = new Student();
+            double status = st.Score = 10;
+            Assert.AreEqual(status, st.Score);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(SystemException))]
+        public void StudentScore_ShouldThrowException()
+        {
+            Student st = new Student();
+            st.Score = 19;
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(SystemException))]
+        public void StudentScore_ShouldThrowException2()
+        {
+            Student st = new Student();
+            st.Score = -19;
+        }
+    }
+}
